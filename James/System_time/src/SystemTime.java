baf397f0ee34dc47f7748179d97c49ab1467fafb
Long24h <vongocphilongrio@gmail.com>
2022-11-08 11:06:49 +0700

    long ml

--- James/System_time/src/SystemTime.java
+++ James/System_time/src/SystemTime.java
@@ -1,5 +1,8 @@
+import java.util.Date;
+
 public class SystemTime {
     public static void main(String[] args) {
-
+        Date cac = new Date();
+        System.out.println("Now is : " + cac);
     }
 }
