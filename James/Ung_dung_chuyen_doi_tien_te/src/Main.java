984dbfd56bad6c38fddbfd65dee7051b7a307b1d
Long24h <vongocphilongrio@gmail.com>
2022-11-14 08:45:26 +0700

    usd

---
+++ James/Ung_dung_chuyen_doi_tien_te/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter USD: ");
+        int USD = scanner.nextInt();
+        int rate = 23000 * USD;
+        System.out.printf("Money change VNĐ: %d VNĐ",rate);
+    }
+}
+
