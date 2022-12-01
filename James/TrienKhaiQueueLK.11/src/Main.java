43dabb03d2b892dd242b894a0597e927ddf21bca
Long24h <vongocphilongrio@gmail.com>
2022-12-01 14:49:31 +0700

    11

---
+++ James/TrienKhaiQueueLK.11/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        String string = "aae bb aa";
+        Stack<Object> stack = new Stack<>();
+        Queue<Object> queue = new LinkedList<>();
+        for (int i = 0; i < string.length(); i++) {
+            stack.push(string.charAt(i));
+        }
+        System.out.println(stack);
+        for (int i = string.length() - 1; i >= 0; i--) {
+            queue.add(string.charAt(i));
+        }
+        System.out.println(queue);
+        if (stack.equals(queue)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
